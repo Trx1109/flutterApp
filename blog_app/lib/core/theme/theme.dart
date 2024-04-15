2664eed9adcacd096de68c3dd2d1f7e0bb404f0f
Nguyen Thanh Trung <trungj6@gmail.com>
2024-04-15 20:50:25 +0700

    implement UI for Login Page

--- blog_app/lib/core/theme/theme.dart
+++ blog_app/lib/core/theme/theme.dart
@@ -2,6 +2,17 @@ import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark()
-      .copyWith(scaffoldBackgroundColor: AppPallete.backgroundColor);
+  static _border({Color color = AppPallete.borderColor}) => OutlineInputBorder(
+      borderSide: BorderSide(
+        color: color,
+        width: 4,
+      ),
+      borderRadius: BorderRadius.circular(10));
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.all(16),
+        enabledBorder: _border(),
+        focusedBorder: _border(color: AppPallete.gradient2)),
+  );
 }
